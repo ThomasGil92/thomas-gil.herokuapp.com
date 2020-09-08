3845b085f58f02447fd80e12c0d4e145360763ea
ThomasGil92 <tgil849@gmail.com>
2020-09-08 18:17:50 +0200

    first test in client react app with jest and enzyme

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+import Home from './components/Home'
+import CV from './components/CV'
+import { MemoryRouter
+} from 'react-router'
+import { Route } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let pathMap = {};
+describe('routes using array of routers', () => {
+  beforeAll(() => {
+    const component = shallow(<App/>);
+    pathMap = component.find(Route).reduce((pathMap, route) => {
+        const routeProps = route.props();
+        pathMap[routeProps.path] = routeProps.component;
+        return pathMap;
+      }, {});
+      /* console.log(pathMap) */
+  })
+  it('should show Home component for / router (getting array of routes)', () => {
+    expect(pathMap['/']).toBe(Home);
+  })
+  it('should show Cv component for /CV router', () => {
+    expect(pathMap['/CV']).toBe(CV);
+  })
+  
+})
