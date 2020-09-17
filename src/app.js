3769d72e37df52b3a9a8daee9dfd541cdc2b836f
ThomasGil92 <tgil849@gmail.com>
2020-09-17 08:40:00 +0200

    user and site routes tests

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname,'client/build')));
 app.use('/public', express.static('public'))
 
  app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html')); // relative path
+  res.sendFile(path.join(__dirname, '/client/build/index.html')); // relative path
 });
 
 
